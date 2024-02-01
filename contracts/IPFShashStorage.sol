32432aeb18d31cb8e225bae771e4e131d9b0dff6
kadie972 <codeurfou@outlook.com>
2024-02-01 14:42:47 +0100

    creation of the smart contract

--- contracts/IPFShashStorage.sol
+++ contracts/IPFShashStorage.sol
@@ -6,6 +6,19 @@ pragma solidity ^0.8.0;
         string fileName;
         string ipfsHash;
     }
+    mapping(string => File) private files;
+
+    function upload (string memory fileName, string memory ipfsHash) public {
+        require(bytes(files[fileName].ipfsHash).length == 0, "file already exsits");
+        files[fileName] = File(fileName, ipfsHash);
+    }
+    function getIpfsHash(string memory fileName) public view returns (string memory) {
+        require(bytes(files[fileName].ipfsHash).length > 0, "file not found");
+        return files[fileName].ipfsHash;
+    }
+    function isFileStored(string memory fileName) public view returns(bool) {
+        return bytes(files[fileName].ipfsHash).length > 0;
+    }
  }
 
 
